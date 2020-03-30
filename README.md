9ef864cdf27819d599441e5c5f11c1db0a2e5054
Martin Davis <martin_davis3@yahoo.ca>
2020-03-30 17:10:01 -0600

    Collect all solutions together

---
+++ README.md
@@ -0,0 +1,86 @@
+Class Assignment: 30-Mar Hackathon
+
+# Sum-of-Peoples-Budgets
+
+https://edabit.com/challenge/tmnCStcrkdWbreKP5
+
+Create the function that takes an array with objects and returns the sum of people's budgets.
+
+Examples
+
+getBudgets([
+
+  { name: "John", age: 21, budget: 23000 },
+  
+  { name: "Steve",  age: 32, budget: 40000 },
+  
+  { name: "Martin",  age: 16, budget: 2700 }
+  
+]) ➞ 65700
+
+getBudgets([
+
+  { name: "John",  age: 21, budget: 29000 },
+  
+  { name: "Steve",  age: 32, budget: 32000 },
+  
+  { name: "Martin",  age: 16, budget: 1600 }
+  
+]) ➞ 62600
+
+
+# Get-Century
+
+https://edabit.com/challenge/SAdqaWKRpjLfZnGKA
+
+Create a function that takes in a year and returns the correct century.
+
+Examples
+
+century(1756) ➞ "18th century"
+
+century(1555) ➞ "16th century"
+
+century(1000) ➞ "10th century"
+
+century(1001) ➞ "11th century"
+
+century(2005) ➞ "21st century"
+
+Notes
+
+    All years will be between 1000 and 2010.
+    
+    The 11th century is between 1001 and 1100.
+    
+    The 18th century is between 1701-1800.
+
+
+# Object-to-Array
+
+https://edabit.com/challenge/4aaBNPnFMc3bzR7JR
+
+Write a function that converts an object into an array of keys and values.
+
+Examples
+
+objectToArray({
+
+  D: 1,
+  
+  B: 2,
+  
+  C: 3
+  
+}) ➞ [["D", 1], ["B", 2], ["C", 3]]
+
+objectToArray({
+
+  likes: 2,
+  
+  dislikes: 3,
+  
+  followers: 10
+  
+}) ➞ [["likes", 2], ["dislikes", 3], ["followers", 10]]
+
---
+++ js/scripts.js
@@ -0,0 +1,109 @@
+
+// Sum of Peoples Budgets
+// https://edabit.com/challenge/tmnCStcrkdWbreKP5
+
+//Class constructor
+class PersonalBudget {
+    constructor ( name, age, budget ) {
+        this.name = name;
+        this.age = age;
+        this.budget = Number( budget );
+    }
+
+    setBudget( budget ) {
+        this.budget = Number( budget );
+    }
+
+}
+
+const getBudgets = ( ...budgetObjs ) => {
+    var budgetSum = 0;
+    for ( let i = 0; i < budgetObjs.length; i++) {
+        var budgetVal = budgetObjs[i].budget;
+        budgetSum += budgetVal;
+    }
+    return budgetSum;    
+}
+
+var johnBudget = new PersonalBudget( 'John', 25, 3000 );
+var jillBudget = new PersonalBudget( 'Jill', 47, 5650 );
+var jeffBudget = new PersonalBudget( 'Jeff', 68, 1625 );
+
+console.log( 'Sum of Peoples Budgets' )
+console.log( johnBudget );
+console.log( jillBudget );
+console.log( jeffBudget );
+console.log( 'Sum:', getBudgets( johnBudget, jillBudget, jeffBudget) );
+console.log();
+
+//Get Century
+// https://edabit.com/challenge/SAdqaWKRpjLfZnGKA
+
+const getCentury = ( year ) => {
+    var ordinalIndicator = 'th';
+    var century = Math.floor( ( year + 99 ) / 100 );
+    if (century > 20 ) { 
+        ordinalIndicator = 'st';
+    }
+    return `${century}${ordinalIndicator} century`;
+}
+
+console.log( 'Get Century' )
+console.log( 'Year 1000:', getCentury( 1000) );
+console.log( 'Year 1701:', getCentury( 1701) );
+console.log( 'Year 2005:', getCentury( 2005) );
+console.log();
+
+// Object to Array
+// https://edabit.com/challenge/4aaBNPnFMc3bzR7JR
+
+class ChemElement {
+    constructor( name, chemSymbol, atomicWeight, wtVariance) {
+    this.name = name;
+    this.chemSymb = chemSymbol;
+    this.atomicWeight = Number(atomicWeight);
+    this.wtVariance = Number(wtVariance);
+    }
+}
+
+//This is the routine as requested for the challenge.
+const objectToArray = ( obj ) => {
+    var objVals = Object.values( obj );
+    var objKeys = Object.keys( obj );
+    var objArray = [];
+    var pairArray = [];
+    for ( let i = 0; i < objKeys.length; i++ ) {
+        pairArray = [ objKeys[ i ] , objVals[ i ] ];
+        objArray.push( pairArray );
+    }
+    return objArray;
+}
+
+//This is perhaps an even better option - putting the key, value pairs into a map
+const objectToMap = ( obj ) => {
+    var objVals = Object.values( obj );
+    var objKeys = Object.keys( obj );
+    var objMap = new Map();
+    for ( let i = 0; i < objKeys.length; i++ ) {
+        objMap.set( objKeys[ i ] , objVals[ i ] );
+    }
+    return objMap;
+}
+
+hydrogen = new ChemElement( 'hydrogen', 'H', 1.00784, 6.075E-09 );
+carbon = new ChemElement( 'carbon', 'C', 12.0096, 3.333E-07 );
+
+
+console.log( 'Object to Array' )
+
+console.log( 'hydrogen object:', hydrogen )
+
+var hArray = objectToArray( hydrogen );
+console.log( 'hydrogen object as array:', hArray );
+
+console.log( 'carbon object:', carbon )
+
+var cMap = objectToMap( carbon );
+console.log( 'carbon object as map:', cMap );
+
+console.log();
